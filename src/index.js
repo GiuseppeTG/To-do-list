3aa202f083814f1195fe06f59d9d75a9b0864751
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-05-31 18:45:42 -0500

    Add listed items with js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,59 @@
 import _ from 'lodash';
 import './style.css';
 
+class Task {
+  constructor(description, author, index) {
+    this.description = description;
+    this.author = author;
+    this.index = index;
+  }
+}
+
+const tasks = [
+  {
+    description: 'item1 dynamic',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'item2 dynamic',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'item3 dynamic',
+    completed: false,
+    index: 2,
+  },
+];
+
+
+const toDoList = (tasks) => {
+  for (let i = 0; i < tasks.length; i += 1) {
+    const list = document.querySelector('.list');
+
+    const item = document.createElement('li');
+    item.classList.add('item');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('checkbox');
+
+    const taskText = document.createElement('p');
+    taskText.classList.add('task-text');
+    taskText.textContent = tasks[i].description;
+
+    const itemAction = document.createElement('i');
+    itemAction.classList.add('fa-solid', 'fa-ellipsis-vertical', 'item-action');
+
+    list.appendChild(item);
+    item.appendChild(checkbox);
+    item.appendChild(taskText);
+    item.appendChild(itemAction);
+    
+  }
+};
+
+toDoList(tasks);
+
 
