166942bf36b50d801e058b883a26762d3dcb7350
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-06-09 16:05:06 -0500

    update editing test

--- src/__test__/edit.test.js
+++ src/__test__/edit.test.js
@@ -4,42 +4,8 @@
 
 import { expect } from '@jest/globals';
  
-let store = {
-  tasks: [
-    {
-      description: 'This is a task',
-      completed: true,
-      index: 0,
-    },
-    {
-      description: 'This is a task again',
-      completed: false,
-      index: 1,
-    },
-  ],
-};
- 
- const mockLocalStorage = (() => ({
-   getItem(key) {
-     return store[key] || null;
-   },
-   setItem(key, value) {
-     store[key] = value.toString();
-   },
-   removeItem(key) {
-     delete store[key];
-   },
-   clear() {
-     store = {};
-   },
- }))();
- 
-describe('Test removing functionalities', () => {
-  beforeAll(() => {
-    Object.defineProperty(window, 'localStorage', {
-      value: mockLocalStorage,
-    });
-  });
+describe('Test edit UI', () => {
+
   test('Edit task description', () => {
    document.body.innerHTML = '<div>'
    + '  <ul class="list">'
